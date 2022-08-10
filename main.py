9a20b409762ce7c4ddfb1c2e414417fa4108491d
jithutagore <53704985+jithutagore@users.noreply.github.com>
2022-08-10 16:38:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+origins = [
+    "http://localhost:50122",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
